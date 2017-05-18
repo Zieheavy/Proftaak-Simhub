b77d661052aabaeec4239305e197a7e8ef71f3f0
Zieheavy <ryanvandenbroek@gmail.com>
2017-05-18 15:18:42 +0200

    updated consolesimhub

--- ConsoleSimHub/ConsoleSimHub/Program.cs
+++ ConsoleSimHub/ConsoleSimHub/Program.cs
@@ -59,41 +59,46 @@ namespace ConsoleSimHub
                 {
                     IPEndPoint anyIP = new IPEndPoint(IPAddress.Any, 20777);
                     byte[] data = client.Receive(ref anyIP);
-                    #region Gear
-                    int pos = 132;
-                    float tTime = (data[pos] & 0xff) + (data[pos + 1] & 0xff) + (data[pos + 2] & 0xff) + (data[pos + 3] & 0xff);
-                    if (tTime == 0)
-                    {
-                        Console.WriteLine("Gear: N");
-                    }
-                    else if (tTime == 64)
-                    {
-                        Console.WriteLine("Gear: 2");
-                    }
-                    else if (tTime == 97)
-                    {
-                        Console.WriteLine("Gear: R");
-                    }
-                    else if (tTime == 128)
-                    {
-                        Console.WriteLine("Gear: 3");
-                    }
-                    else if (tTime == 191)
-                    {
-                        Console.WriteLine("Gear: 1");
-                    }
-                    else if (tTime == 192)
-                    {
-                        Console.WriteLine("Gear: 4");
-                    }
-                    else if (tTime == 224)
-                    {
-                        Console.WriteLine("Gear: 5");
-                    }
-                    else
-                    {
-                        Console.WriteLine(tTime);
-                    }
+                    //#region Gear
+                    //int posGear = 132;
+                    //float tTime = (data[posGear] & 0xff) + (data[posGear + 1] & 0xff) + (data[posGear + 2] & 0xff) + (data[posGear + 3] & 0xff);
+                    //if (tTime == 0)
+                    //{
+                    //    Console.WriteLine("Gear: N");
+                    //}
+                    //else if (tTime == 64)
+                    //{
+                    //    Console.WriteLine("Gear: 2");
+                    //}
+                    //else if (tTime == 97)
+                    //{
+                    //    Console.WriteLine("Gear: R");
+                    //}
+                    //else if (tTime == 128)
+                    //{
+                    //    Console.WriteLine("Gear: 3");
+                    //}
+                    //else if (tTime == 191)
+                    //{
+                    //    Console.WriteLine("Gear: 1");
+                    //}
+                    //else if (tTime == 192)
+                    //{
+                    //    Console.WriteLine("Gear: 4");
+                    //}
+                    //else if (tTime == 224)
+                    //{
+                    //    Console.WriteLine("Gear: 5");
+                    //}
+                    //else
+                    //{
+                    //    Console.WriteLine(tTime);
+                    //}
+                    //#endregion
+                    #region 28
+                    int posSpeed = 132;
+                    float speed = (data[posSpeed] & 0xff) + (data[posSpeed + 1] & 0xff) + (data[posSpeed + 2] & 0xff) + (data[posSpeed + 3] & 0xff);
+                    Console.WriteLine(">>" +speed);
                     #endregion
                 }
                 catch (Exception err)
